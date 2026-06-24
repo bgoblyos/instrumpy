0538dcc7b65ed696ab2214c373f731bd0b7c186c
Bence Goblyos <bence@gobyos.dev>
2026-06-24 16:25:55 -0400

    Update Luminator
    Rework config handling and add safety timeout.
    
    Signed-off-by: Bence Goblyos <bence@gobyos.dev>

--- Devices/Luminator.py
+++ Devices/Luminator.py
@@ -14,8 +14,10 @@ this program. If not, see https://www.gnu.org/licenses/.
 """
 
 import numpy as np
-import pandas as pd
+import json
 import logging
+import time
+from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 
@@ -26,7 +28,7 @@ class Luminator():
     Handles instrument communication via PyVISA and applies an optional
     linear wavelength calibration based on a provided configuration file.
     """
-    def __init__(self, rm, address, config = (PROJECT_ROOT / "Config" / "Luminator.csv")):
+    def __init__(self, rm, address, config = (PROJECT_ROOT / "Config" / "Luminator.json")):
         """
         Initializes the monochromator connection and loads calibration data.
 
@@ -50,15 +52,20 @@ class Luminator():
         self.device.write_termination = "\r\n"
         self.device.read_termination = "\r\n"
 
-        try:
-            df = pd.read_csv(config)
-            [a, b] = np.polyfit(df.setpoint, df.peak, 1)
-        except:
-            self.logger.warning("Could not load calibration dataset, assuming identity transfer function.")
-            [a, b] = [1.0, 0.0]
-        finally:
-            self.a = a
-            self.b = b
+        d = {}
+        if config is not None:
+            try:
+                with open(config, "r") as file:
+                    d = json.load(file)
+            except Exception as e:
+                self.logger.warning(f"Configuration could not be loaded: {e}")
+                d = {}
+
+        self.a = d.get('c1', 1.0)
+        self.b = d.get('c0', 0.0)
+        self.lower = d.get('lower', 300.0)
+        self.upper = d.get('upper', 1200.0)
+        self.speed = d.get('speed', 50.0)
 
     def internal2actual(self, internal):
         """
@@ -74,7 +81,7 @@ class Luminator():
         actual: float
             The calibrated actual output wavelength in nm.
         """
-        return self.a*internal + self.b
+        return float(self.a*internal + self.b)
 
     def actual2internal(self, actual):
         """
@@ -90,7 +97,7 @@ class Luminator():
         internal: float
             The necessary internal setpoint in nm to achieve the actual wavelength.
         """
-        return (actual - self.b)/self.a
+        return float((actual - self.b)/self.a)
 
     def setWavelength(self, target, convert = True):
         """
@@ -103,9 +110,22 @@ class Luminator():
         convert: bool, default: True
             If True, applies the linear calibration to the target before sending.
         """
+        old = self.getWavelength(convert = convert)
+
+        if target < self.lower:
+            target = self.lower
+            self.logger.warning(f"Wavelength below minimum level. It has been increased to {self.lower} nm.")
+
+        if target > self.upper:
+            target = self.upper
+            self.logger.warning(f"Wavelength above maximum level. It has been decreased to {self.upper} nm.")
+
+        wait = (np.abs(old - target) / self.speed) + 0.05
         target = self.actual2internal(target) if convert else target
+        
         cmd = "GOWAVE " + "{:.3f}".format(target)
         self.device.write(cmd)
+        time.sleep(wait)
 
     def getWavelength(self, convert = True):
         """
